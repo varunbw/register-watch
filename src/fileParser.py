e010a09d19aca969336a29b5ca9f2c181f7e20bd
HaloArin7 <arinkhopkar@gmail.com>
2024-03-09 22:02:09 +0530

    More functionalities added

--- src/fileParser.py
+++ src/fileParser.py
@@ -1,33 +1,61 @@
 import time
 
-line_iter = 0
-with open('addNum.asm', 'r') as file:
+with open('../asm/asmFile.asm', 'r') as file:
     
-    line_iter += 1
-
     for line in file:
+
         line = line.strip()
         words = line.split()
-        if(words[0][0] == '%'):
-            instruction = words[0].strip()
-            var1 = words[1].strip()
-            var2 = words[2].strip()
-            
-        elif (',' in line):
-            instruction = words[0]
-            comma_index = line.index(',')
-            var1 = line[len(instruction) + 1:comma_index].strip()
-            var2 = line[comma_index + 1:].strip()
+        instruction = ''
+        var1 = ''
+        var2 = ''
+        if(words != []):
+            if(words[0] == '%macro'):
+                instruction = words[0].strip()
+                var1 = words[1].strip()
+                var2 = words[2].strip()
+                    
+            elif(words[0] == '%endmacro'):
+                instruction = words[0].strip()
+                var1 = None
+                var2 = None
 
-            print("Instruction:", instruction)
-            print("Variable 1:", var1)
-            print("Variable 2:", var2)
+            elif(words[0] == 'section'):
+                
+                instruction = words[0].strip()
+                var1 = words[1].strip()
+                var2 = None
 
+            elif(words[0] == 'global'):
+                instruction = words[0].strip()
+                var1 = words[1].strip()
+                var2 = None
+
+            elif(words[0][len(words[0])-1] == ':'):
+                instruction = words[0]
+                var1 = None
+                var2 = None
+
+                
+
+            elif (',' in line):
+                instruction = words[0]
+                comma_index = line.index(',')
+                var1 = line[len(instruction) + 1:comma_index].strip()
+                var2 = line[comma_index + 1:].strip()
+
+                
+            list_of_inputs = []
+            list_of_inputs.append(instruction)
+            list_of_inputs.append(var1)
+            list_of_inputs.append(var2)
+                
+
+            print("[Instruction, Variable 1, Variable 2]:\n", list_of_inputs)
             print("Press Enter to continue:")
             input()
-
-        
-
+        else:
+            continue
 
         
             
